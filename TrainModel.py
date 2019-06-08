2f5113d10b2ad6b0545fc65d7c0fe4933d71fb91
Ian Battin <ianbattin0@gmail.com>
2019-06-08 12:51:30 -0700

    Finished re organizing stuff. Starting on testing

--- TrainModel.py
+++ TrainModel.py
@@ -1,13 +1,15 @@
+import sys
 from keras import layers
 from keras import models
 from keras.applications.inception_v3 import InceptionV3
 from keras.preprocessing.image import ImageDataGenerator
 
 
-def get_generators(train_dir_name, val_dir_name):
+def get_generators(train_dir_name, val_dir_name, test_dir_name):
     # All images will be rescaled by 1./255
     train_data_gen = ImageDataGenerator(rescale=1./255)
     val_data_gen = ImageDataGenerator(rescale=1./255)
+    test_data_gen = ImageDataGenerator(rescale=1./255)
 
     train_generator = train_data_gen.flow_from_directory(
         train_dir_name,
@@ -19,13 +21,19 @@ def get_generators(train_dir_name, val_dir_name):
         target_size=(299, 299),
         batch_size=32,
         class_mode="categorical")
+    test_generator = test_data_gen.flow_from_directory(
+        test_dir_name,
+        target_size=(299, 299),
+        batch_size=1,
+        class_mode=None
+    )
 
     for data_batch, labels_batch in train_generator:
         print('data batch shape:', data_batch.shape)
         print('labels batch shape:', labels_batch.shape)
         break
 
-    return train_generator, val_generator
+    return train_generator, val_generator, test_generator
 
 
 def build_model():
@@ -58,21 +66,36 @@ def train_model(model, train_generator, val_generator, verbose=False):
         verbose=verbose)
 
 
-def test_model(model, test_dir_name):
-    print("TODO")
+def test_model(model, test_generator):
+    steps = test_generator.n // test_generator.batch_size
+
+    predictions = model.predict_generator(
+        test_generator,
+        steps=steps,
+        verbose=True)
+
+    print(predictions)
 
 
-def main():
+def main(existing_model_path=None):
     train_dir_name = 'train/'
     val_dir_name = 'validation/'
     test_dir_name = 'test/'
 
-    train_generator, val_generator = get_generators(
-        train_dir_name, val_dir_name)
+    train_generator, val_generator, test_generator = get_generators(
+        train_dir_name, val_dir_name, test_dir_name)
 
-    model = build_model()
+    if existing_model_path is not None:
+        model = models.load_model(existing_model_path)
+    else:
+        model = build_model()
+        train_model(model, train_generator, val_generator, verbose=True)
+        model.save("./Model.h5")
 
-    train_model(model, train_generator, val_generator, verbose=True)
+    test_model(model, test_generator)
 
 
-main()
+if len(sys.argv) > 1:
+    main(sys.argv[1])
+else:
+    main()
