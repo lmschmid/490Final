cfa196efce581aa1a2e38c7c0d4d291babbd9ff7
Ian Battin <ianbattin0@gmail.com>
2019-06-08 12:30:14 -0700

    Moved code into functions for better organization

--- TrainModel.py
+++ TrainModel.py
@@ -4,48 +4,75 @@ from keras.applications.inception_v3 import InceptionV3
 from keras.preprocessing.image import ImageDataGenerator
 
 
-trainDirName = 'train/'
-valDirName = 'validation/'
-testDirName = 'test/'
+def get_generators(train_dir_name, val_dir_name):
+    # All images will be rescaled by 1./255
+    train_data_gen = ImageDataGenerator(rescale=1./255)
+    val_data_gen = ImageDataGenerator(rescale=1./255)
 
-# All images will be rescaled by 1./255
-trainDatagen = ImageDataGenerator(rescale=1./255)
-valDatagen = ImageDataGenerator(rescale=1./255)
+    train_generator = train_data_gen.flow_from_directory(
+        train_dir_name,
+        target_size=(299, 299),
+        batch_size=32,
+        class_mode="categorical")
+    val_generator = val_data_gen.flow_from_directory(
+        val_dir_name,
+        target_size=(299, 299),
+        batch_size=32,
+        class_mode="categorical")
 
-trainGenerator = trainDatagen.flow_from_directory(
-    trainDirName,
-    target_size=(299, 299),
-    batch_size=32,
-    class_mode="categorical")
-valGenerator = trainDatagen.flow_from_directory(
-    valDirName,
-    target_size=(299, 299),
-    batch_size=32,
-    class_mode="categorical")
+    for data_batch, labels_batch in train_generator:
+        print('data batch shape:', data_batch.shape)
+        print('labels batch shape:', labels_batch.shape)
+        break
 
-for data_batch, labels_batch in trainGenerator:
-    print('data batch shape:', data_batch.shape)
-    print('labels batch shape:', labels_batch.shape)
-    break
+    return train_generator, val_generator
 
 
-convBase = InceptionV3(
-    weights='imagenet', include_top=False, input_shape=(299, 299, 3))
-convBase.trainable = False
+def build_model():
+    convBase = InceptionV3(
+        weights='imagenet', 
+        include_top=False, 
+        input_shape=(299, 299, 3))
+    convBase.trainable = False
 
-model = models.Sequential()
-model.add(convBase)
-model.add(layers.Flatten())
-model.add(layers.Dense(256, activation='relu'))
-model.add(layers.Dense(120, activation='softmax'))
+    model = models.Sequential()
+    model.add(convBase)
+    model.add(layers.Flatten())
+    model.add(layers.Dense(256, activation='relu'))
+    model.add(layers.Dense(120, activation='softmax'))
 
-model.compile(optimizer='rmsprop',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+    model.compile(optimizer='rmsprop',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
 
-history = model.fit_generator(
-    trainGenerator,
-    steps_per_epoch=100,
-    epochs=30,
-    validation_data=valGenerator,
-    validation_steps=50)
+    return model
+
+
+def train_model(model, train_generator, val_generator, verbose=False):
+    history = model.fit_generator(
+        train_generator,
+        steps_per_epoch=100,
+        epochs=30,
+        validation_data=val_generator,
+        validation_steps=50,
+        verbose=verbose)
+
+
+def test_model(model, test_dir_name):
+    print("TODO")
+
+
+def main():
+    train_dir_name = 'train/'
+    val_dir_name = 'validation/'
+    test_dir_name = 'test/'
+
+    train_generator, val_generator = get_generators(
+        train_dir_name, val_dir_name, test_dir_name)
+
+    model = build_model(include_top=True)
+
+    train_model(model, train_generator, val_generator, verbose=True)
+
+
+main()
