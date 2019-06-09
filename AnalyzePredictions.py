ff3c019dfcd0334a8703fd111ac146d57d9d8100
Ian Battin <ianbattin0@gmail.com>
2019-06-08 18:31:19 -0700

    Fixed the analysis

--- AnalyzePredictions.py
+++ AnalyzePredictions.py
@@ -1,6 +1,7 @@
 import sys
 import pandas as pd
 
+
 def main(predictions_file):
     df = pd.read_csv(predictions_file)
 
@@ -8,10 +9,15 @@ def main(predictions_file):
     headers = list(df)
     
     for index, row in df.iterrows():
-        sorted_by_breed = \
-          sorted(list(zip(headers, row[1:].tolist())), key=lambda x: x[1], reverse=True)
-        print([x for x,y in sorted_by_breed[:5]])
-        sorted_labels[row[0]] = sorted_by_breed
+        confidence_vals = map(float, row[1:].tolist())
+        zipped = list(zip(headers[1:], confidence_vals))
+        sorted_zip = sorted(zipped, reverse=True, key=lambda x: x[1])
+        sorted_labels[row[0]] = sorted_zip
+
+        top_5_breeds = [breed for breed,conf in sorted_zip[:5]]
+
+        print("Top 5 breeds for image '{}': {}".format(row[0], ", ".join(top_5_breeds)))
+
           
 
 main(sys.argv[1])
